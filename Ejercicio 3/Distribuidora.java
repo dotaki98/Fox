8ff95ca09ec9b3ae0b3d142a8d78dfa441e0fe27
Ulises Landázuri <u.landazuri.98@gmail.com>
2017-09-11 00:06:08 -0500

    Comenzar codificación clase Distribuidora

--- Ejercicio 3/Distribuidora.java
+++ Ejercicio 3/Distribuidora.java
@@ -1,3 +1,18 @@
-public class Distribuidora{
+import java.util.Random;
+import java.util.ArrayList;
 
+public class Distribuidora{
+	private ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+	private Fabrica fabrica = new Fabrica("Fabrica de autos", "Av. del IMAN 704");
+	Random rnd = new Random();
+	public static void main(String[] args){
+		int numero_de_autos = ((rnd.nextInt()) % 4)+1;
+		int numero_de_motos = 5 - numero_de_autos;
+		for(int i = 1; i<= numero_de_autos; i++) {
+			vehiculos.add(fabrica.fabricarVehiculo(1));
+			
+		}
+		for(int i = 1; i<= numero_de_motos; i++) {
+			vehiculos.add(fabrica.fabricarVehiculo(0));
+		}
 }
