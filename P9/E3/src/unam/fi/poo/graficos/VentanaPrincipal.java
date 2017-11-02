0bc405142bb60408ab8b75fb5a50ad8277bd3e19
Ulises Landázuri <ulises@Ulises-Mac.local>
2017-11-01 20:35:31 -0600

    Agregar ejemplos de lectura y escritura de archivos

--- P9/E3/src/unam/fi/poo/graficos/VentanaPrincipal.java
+++ P9/E3/src/unam/fi/poo/graficos/VentanaPrincipal.java
@@ -8,13 +8,12 @@ import java.awt.event.*;
 
 public class VentanaPrincipal extends JFrame implements ActionListener{
 	// Paneles auxiliares
-	JPanel panelNorte;	
-	JPanel panelSur;	
+	JPanel panelSuperior;
 	JPanel panelCentro;	
-	JPanel panelEste;	
-	JPanel panelOeste;	
-
-	BorderLayout layout = new BorderLayout();
+	JPanel panelInferior;
+	// Creación del layout
+	GridBagLayout layout = new GridBagLayout();
+	GridBagConstraints constraints = new GridBagConstraints();
 
 	public VentanaPrincipal() {
 		super();		
@@ -25,62 +24,131 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 
 	public void crearVentanaPrincipal(){
 		this.setTitle("SCD");
-		this.setSize(500, 400);
-		this.setLayout(layout);
-		this.layout.setVgap(20);
-		this.layout.setHgap(20);
+		this.setSize(350, 300);
+		this.setLayout(new BorderLayout());
 		this.setResizable(false);
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-
-		// Crear paneles
-		this.panelNorte = new JPanel(new FlowLayout());
-		this.panelSur = new JPanel(new FlowLayout());
-		this.panelCentro = new JPanel(new FlowLayout());
-		this.panelEste = new JPanel(new FlowLayout());
-		this.panelOeste = new JPanel(new FlowLayout());
-
-		// Colorear cada panel
-		//this.panelNorte.setBackground(Color.red);
-		//this.panelCentro.setBackground(Color.cyan);
-		//this.panelSur.setBackground(Color.green);
-		//this.panelEste.setBackground(Color.blue);
-		//this.panelOeste.setBackground(Color.yellow);
-		
-		// Agregar paneles a la panel principal
-		this.add(this.panelNorte, BorderLayout.NORTH);
-		this.add(this.panelCentro, BorderLayout.CENTER);
-		this.add(this.panelSur, BorderLayout.SOUTH);
-		this.add(this.panelEste, BorderLayout.EAST);
-		this.add(this.panelOeste, BorderLayout.WEST);
 	}
 
 	public void agregarComponentes(){
 		JLabel etiquetaSuperior = new JLabel("Sistema de Captura de Datos");
-		JLabel etiquetaNombre = new JLabel("Nombre: ");
-		JLabel etiquetaApellidoPaterno = new JLabel("Apellido Paterno: ");
-		JLabel etiquetaApellidoMaterno = new JLabel("Apellido Materno: ");
-		JLabel etiquetaEdad = new JLabel("Edad: ");
-		JLabel etiquetaEstadoActual = new JLabel("¡Bienvenido al Sistema!");
-		JTextField cajaNombre = new JTextField(10);
-		JTextField cajaApellidoPaterno = new JTextField(10);
-		JTextField cajaApellidoMaterno = new JTextField(10);
-		JTextField cajaEdad = new JTextField(10);
-		JButton botonCancelar = new JButton("Cancelar");
-		JButton botonBorrar = new JButton("Borrar");
-		JButton botonAceptar = new JButton("Aceptar");
+		//constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.anchor = GridBagConstraints.PAGE_START;
+		constraints.gridwidth = 6;
+		constraints.weighty = 1;
+		panelSuperior = new JPanel(new FlowLayout());
+		panelSuperior.setBackground(Color.white);
+		panelSuperior.setLayout(layout);
+		panelSuperior.add(etiquetaSuperior, constraints);
+		constraints.gridwidth = 1;
+		constraints.weighty = 0;
+		constraints.anchor = GridBagConstraints.CENTER;
 
+		JLabel etiquetaNombre = new JLabel("Nombre:", SwingConstants.CENTER);
+		//constraints.fill = GridBagConstraints.NONE;
+		//constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.gridx = 0;
+		constraints.gridy = 3;
+		constraints.anchor = GridBagConstraints.LINE_END;
+		constraints.weightx = 1;
+		constraints.weightx = 0;
+		constraints.gridwidth = 3;
+		panelSuperior.add(etiquetaNombre, constraints);
+		constraints.fill = GridBagConstraints.NONE;
+
+		JTextField cajaNombre = new JTextField(10);
+		//constraints.fill = GridBagConstraints.NONE;
+		constraints.gridx = 3;
+		constraints.gridy = 3;
+		//constraints.weightx = 1;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panelSuperior.add(cajaNombre, constraints);
+
+		JLabel etiquetaApellidoPaterno = new JLabel("Apellido Paterno:");
+		//constraints.fill = GridBagConstraints.NONE;
+		constraints.gridx = 0;
+		constraints.gridy = 4;
+		constraints.anchor = GridBagConstraints.LINE_END;
+		//constraints.gridwidth = 2;
+		panelSuperior.add(etiquetaApellidoPaterno, constraints);
+
+		JTextField cajaApellidoPaterno = new JTextField(10);
+		//constraints.fill = GridBagConstraints.NONE;
+		constraints.gridx = 3;
+		constraints.gridy = 4;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panelSuperior.add(cajaApellidoPaterno, constraints);
+
+		JLabel etiquetaApellidoMaterno = new JLabel("Apellido Materno:");
+		constraints.gridx = 0;
+		constraints.gridy = 5;
+		constraints.anchor = GridBagConstraints.LINE_END;
+		panelSuperior.add(etiquetaApellidoMaterno, constraints);
+
+		JTextField cajaApellidoMaterno = new JTextField(10);
+		constraints.gridx = 3;
+		constraints.gridy = 5;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panelSuperior.add(cajaApellidoMaterno, constraints);
+
+		JLabel etiquetaEdad = new JLabel("Edad:");
+		constraints.gridx = 0;
+		constraints.gridy = 6;
+		constraints.anchor = GridBagConstraints.LINE_END;
+		panelSuperior.add(etiquetaEdad, constraints);
+
+		JTextField cajaEdad = new JTextField(10);
+		constraints.gridx = 3;
+		constraints.gridy = 6;
+		constraints.anchor = GridBagConstraints.CENTER;
+		panelSuperior.add(cajaEdad, constraints);
+		this.add(panelSuperior, BorderLayout.NORTH);
+
+		panelCentro = new JPanel(new FlowLayout());
+		panelCentro.setBackground(Color.gray);
+		constraints.weighty = 1;
+
+		constraints.gridwidth = 2;
+		constraints.anchor = GridBagConstraints.PAGE_END;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		JButton botonCancelar = new JButton("Cancelar");
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.weightx = 1;
+		panelCentro.add(botonCancelar, constraints);
+
+		JButton botonBorrar = new JButton("Borrar");
+		constraints.gridwidth = 2;
+		constraints.gridx = 2;
+		constraints.gridy = 0;
+		panelCentro.add(botonBorrar, constraints);
+
+		JButton botonAceptar = new JButton("Aceptar");
+		constraints.gridx = 4;
+		constraints.gridy = 0;
+		panelCentro.add(botonAceptar, constraints);
+		this.add(panelCentro, BorderLayout.CENTER);
+
+		panelInferior = new JPanel(new FlowLayout());
+		panelInferior.setBackground(Color.white);
+		constraints.fill = GridBagConstraints.NONE;
+		constraints.anchor = GridBagConstraints.CENTER;
+		JLabel etiquetaEstadoActual = new JLabel("¡Bienvenido al Sistema!");
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.gridwidth = 6;
+		constraints.gridheight = 3;
+		constraints.weighty = 1;
+		panelInferior.add(etiquetaEstadoActual, constraints);
+		constraints.gridwidth = 1;
+		constraints.gridheight = 1;
+		this.add(panelInferior, BorderLayout.SOUTH);
 		// Agregar listeners a botones
 		botonCancelar.addActionListener(this);
 		botonBorrar.addActionListener(this);
 		botonAceptar.addActionListener(this);
-
-		// Agregar componentes a panel principal
-		this.panelNorte.add(etiquetaSuperior);
-		this.panelCentro.add(etiquetaNombre);
-		this.panelCentro.add(cajaNombre);
-
-				
 	}	
 	
 	public void actionPerformed(ActionEvent e){
