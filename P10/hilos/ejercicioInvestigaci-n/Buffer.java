61e7cc37f9c7423597592844a5bdfcf1c1a861e8
fernando170 <fer-170@live.com.mx>
2017-11-12 21:44:25 -0600

    Update Buffer.java

--- P10/hilos/ejercicioInvestigaci-n/Buffer.java
+++ P10/hilos/ejercicioInvestigaci-n/Buffer.java
@@ -1,22 +1,8 @@
-//package thread2;
-
 public class Buffer {
   private char contenido;
   private boolean disponible=false;
   public Buffer() {
   }
-/*  public char recoger(){
-    if(disponible){
-        disponible=false;
-        return contenido;
-    }
-    return ('\t');
-  }
-  public void poner(char c){
-    contenido=c;
-    disponible=true;
-  }     */
-
   public synchronized char recoger(){
     while(!disponible){
         try{
