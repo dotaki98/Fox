c1cc3cf544a46168288e98d577b39bb9ff18116e
fernando170 <fer-170@live.com.mx>
2017-11-12 22:44:49 -0600

    Add files via upload

---
+++ P10/hilos/ejercicioInvestigaci-n/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		// Objeto en comun, se encarga del wait y notify
+		Saludo s = new Saludo();
+		Personal Empleado1 = new Personal("Pepe", s, false);
+		Personal Empleado2 = new Personal("José", s, false);
+		Personal Empleado3 = new Personal("Pedro", s, false);
+		Personal Jefe1 = new Personal("JEFE", s, true);
+		Empleado1.start();
+		Empleado2.start();
+		Empleado3.start();
+		Jefe1.start();
+	}
+}
