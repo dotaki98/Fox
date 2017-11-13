e0703cc6e2ac66cd6594215db24b55b0b2d00d51
fernando170 <fer-170@live.com.mx>
2017-11-12 22:50:10 -0600

    Delete CampoDeTiro.java

--- P10/hilos/ejercicioInvestigaci-n/CampoDeTiro.java
+++
@@ -1,9 +0,0 @@
-public class CampoDeTiro {
-    public static void main(String[] args) {
-        Pistola arma = new Pistola();
-        Cargar c = new Cargar(arma, 1);
-        Descargar d = new Descargar(arma, 1);
-        c.start();
-        d.start();
-    }
- }
